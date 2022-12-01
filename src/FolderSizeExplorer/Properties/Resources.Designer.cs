c23a7c7857cf25d97e5a425e78304eb9098a44e0
unknown <karmaelin8@gmail.com>
2022-12-01 15:07:25 +0300

    proj created

--- src/FolderSizeExplorer/Properties/Resources.Designer.cs
+++ src/FolderSizeExplorer/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FolderSize.Properties
+namespace FolderSizeExplorer.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace FolderSize.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FolderSize.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FolderSizeExplorer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- src/FolderSizeExplorer/Properties/Settings.Designer.cs
+++ src/FolderSizeExplorer/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FolderSize.Properties
+namespace FolderSizeExplorer.Properties
 {
 
 
