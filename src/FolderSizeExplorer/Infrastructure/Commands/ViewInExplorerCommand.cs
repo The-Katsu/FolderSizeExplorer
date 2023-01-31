be230e6b060bd7670f01776985a99cd2af57088a
unknown <karmaelin8@gmail.com>
2023-01-31 22:10:37 +0300

    view in explorer context menu command

---
+++ src/FolderSizeExplorer/Infrastructure/Commands/ViewInExplorerCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using FolderSizeExplorer.Infrastructure.Commands.Base;
+using FolderSizeExplorer.Models.Base;
+using FolderSizeExplorer.Services.Helpers;
+
+namespace FolderSizeExplorer.Infrastructure.Commands
+{
+    internal class ViewInExplorerCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter)
+        {
+            var row = parameter as DataGridRow;
+            var item = row?.Item as File;
+            ExplorerService.OpenWithSelect(item?.Path);
+        }
+    }
+}
